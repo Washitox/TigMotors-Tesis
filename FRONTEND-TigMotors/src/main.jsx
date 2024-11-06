10e9217951808b586742f1b0014d6ed62594801a
Washitox <washovilla78@gmail.com>
2024-11-05 23:04:27 -0500

    Subida carpetas Frontend

--- FRONTEND-TigMotors/src/main.jsx
+++ FRONTEND-TigMotors/src/main.jsx
@@ -1,10 +1,15 @@
+import { Theme } from "@radix-ui/themes"; 
+import "@radix-ui/themes/styles.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    <Theme>
+      <App />
+    </Theme>
+      
 )
+
