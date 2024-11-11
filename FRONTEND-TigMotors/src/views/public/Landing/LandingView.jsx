1fb531823651b0b4fdb33f4ea0666bc5adbfe050
Washitox <washovilla78@gmail.com>
2024-11-11 14:12:20 -0500

    Actualización de Landing

--- FRONTEND-TigMotors/src/views/public/Landing/LandingView.jsx
+++ FRONTEND-TigMotors/src/views/public/Landing/LandingView.jsx
@@ -1,9 +1,12 @@
 import Section from "./ui/Section"
+import Videointroduccion from "./ui/Videointroduccion"
+
 
 export default function LandingView() {
   return (
     <div>
       <Section/>
+      <Videointroduccion/>  
     </div>
   )
 }
